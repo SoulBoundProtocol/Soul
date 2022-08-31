89091daa771519aa729ef117985463f8d6dc6ede
Li, Amazing Ang <d0u.gie@hotmail.com>
2022-08-31 22:58:35 +0800

    update

--- SOAPMinter.sol
+++ SOAPMinter.sol
@@ -3,24 +3,24 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "./sPOAP.sol";
+import "./SOAP.sol";
 
-contract sPOAPMinter is Ownable{
+contract SOAPMinter is Ownable{
     /* ============ Events ============ */
-    event sPOAPMinted(address indexed account, uint256 indexed eventId);
+    event SOAPMinted(address indexed account, uint256 indexed eventId);
     event SignerChanged(address indexed oldSigner, address indexed newSigner);
 
     /* ============ State Variables ============ */
     address public signer;
-    sPOAP public poap;
+    SOAP public soap;
     mapping(uint256 => mapping(address => bool)) public mintedAddress;   // 记录已经mint的地址
 
     /* ============ Constructor ============ */
     /*
-     * @dev initialize sPOAP addresss and signer address
+     * @dev initialize SOAP addresss and signer address
      */
-    constructor(address sPOAPaddr_, address signer_){
-        poap = sPOAP(sPOAPaddr_);
+    constructor(address soapAddr_, address signer_){
+        soap = SOAP(soapAddr_);
         signer = signer_;
     }
 
@@ -56,16 +56,16 @@ contract sPOAPMinter is Ownable{
         bytes32 ethSignedMessageHash = ECDSA.toEthSignedMessageHash(msgHash); // 计算以太坊签名消息
         require(verify(ethSignedMessageHash, signature), "Invalid signature"); // ECDSA检验通过
         require(!mintedAddress[eventId][account], "Already minted!"); // 地址没有mint过
-        poap.mint(account, eventId); // mint
+        soap.mint(account, eventId); // mint
         mintedAddress[eventId][account] = true; // 记录mint过的地址
-        emit sPOAPMinted(account, eventId);
+        emit SOAPMinted(account, eventId);
     }
 
     /**
      * @dev change signer address. Only owner can call.
      * @param newSigner: address of new signer
      */
-     function setSigner(address newSigner) external{
+     function setSigner(address newSigner) external onlyOwner{
          emit SignerChanged(signer, newSigner);
          signer = newSigner;
      }
